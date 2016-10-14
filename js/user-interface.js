6e456515488dde9a56bcdb7aefd3fdd70cdcd74b
Sara Jay <jensese@MacBook-Pro-2.local>
2016-10-14 10:00:59 -0700

    Use callback functions to display user data and repos in the front end. Add getRepos function that displays the url of the most recent 30 repos created by a user.

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,11 +1,20 @@
 var User = require('./../js/user.js').userModule;
 var getRepos = require('./../js/user.js').getRepos;
 
+var displayUserData = function(name, location, email) {
+  $('#profile-results').text(name + ' | ' + location + ' | ' + email + ' | ');
+};
+
+var displayRepos = function(archive_url) {
+  $('#repo-results').append('<li>' + archive_url + '</li>');
+};
+
 $(document).ready(function() {
   $('#user-search-form').submit(function(event){
     event.preventDefault();
-    $('#search-results').text("Yo it's working, move on.");
+    var username = $('#username').val();
     var newUser = new User();
-    newUser.getRepos();
+    newUser.getUserProfile(username, displayUserData);
+    newUser.getRepos(username, displayRepos);
   });
 });
--- js/user.js
+++ js/user.js
@@ -3,8 +3,22 @@ var apiKey = require('./../.env').apiKey;
 User = function(){
 };
 
-User.prototype.getRepos = function(){
-  $.get('https://api.github.com/users/thejensen?access_token=' + apiKey).then(function(response){
+User.prototype.getUserProfile = function(username, displayUserData){
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    displayUserData(response.name, response.location, response.email);
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+User.prototype.getRepos = function(username, displayRepos){
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+
+    for(i=0; i<response.length; i++) {
+      displayRepos(response[i].archive_url);
+    }
+
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
