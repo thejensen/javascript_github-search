3496dd11289799ed783a0c06364464199645bae1
Sara Jay <jensese@MacBook-Pro-2.local>
2016-10-14 09:21:32 -0700

    Add User function to make an API call to github with my api public access token to retrieve user data provided by github in json format.

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,8 +1,11 @@
 var User = require('./../js/user.js').userModule;
+var getRepos = require('./../js/user.js').getRepos;
 
 $(document).ready(function() {
   $('#user-search-form').submit(function(event){
     event.preventDefault();
     $('#search-results').text("Yo it's working, move on.");
+    var newUser = new User();
+    newUser.getRepos();
   });
 });
--- js/user.js
+++ js/user.js
@@ -1,7 +1,14 @@
-var apiPAT = require('./../.env').apiPAT;
+var apiKey = require('./../.env').apiKey;
 
 User = function(){
 };
 
+User.prototype.getRepos = function(){
+  $.get('https://api.github.com/users/thejensen?access_token=' + apiKey).then(function(response){
+    console.log(response);
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
 
 exports.userModule = User;
