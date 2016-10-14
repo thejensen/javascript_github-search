8ae1fcc77de6fd5dbc6d6600a51c27add6bc501e
Sara Jay <jensese@MacBook-Pro-2.local>
2016-10-14 11:53:25 -0700

    Show error messages if a user is not found, and if a user has no repos. Add repo create date to repo details, formatted for human consumption.

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,20 +1,29 @@
 var User = require('./../js/user.js').userModule;
 
-var displayUserData = function(name, location, email) {
-  $('#profile-results').text(name + ' | ' + location + ' | ' + email);
+var displayUserData = function(name, location, email, htmlUrl) {
+  $('#profile-results').append('<div class="col-sm-4">' + name + '</div><div class="col-sm-4">' + location + '</div><div class="col-sm-4">' + email + '</div><br>' + htmlUrl + '<hr>');
 };
 
-var displayRepos = function(url, name, description) {
-  $('#repo-results').append('<li>' + name + '<br>' + description + '<br>' + url + '</li>');
+var displayRepos = function(name, language, created, description, url) {
+  $('#repo-results').append('<li><span id="name">' + name + ' | ' + language + '</span><br>' + created + '<br>' + description + '<br>' + url + '</li>');
+};
+
+var userFail = function(message) {
+  $('#profile-results').append('Sorry! User ' + message);
+};
+
+var reposFail = function(message) {
+  $('#repo-results').append('Hmm, looks like are no repos here.');
 };
 
 $(document).ready(function() {
   $('#user-search-form').submit(function(event){
     event.preventDefault();
+    $("#profile-results").html("");
     $("#repo-results").html("");
     var username = $('#username').val();
     var newUser = new User();
-    newUser.getUserProfile(username, displayUserData);
-    newUser.getRepos(username, displayRepos);
+    newUser.getUserProfile(username, displayUserData, userFail);
+    newUser.getRepos(username, displayRepos, reposFail);
   });
 });
--- js/user.js
+++ js/user.js
@@ -3,25 +3,23 @@ var apiKey = require('./../.env').apiKey;
 User = function(){
 };
 
-User.prototype.getUserProfile = function(username, displayUserData){
+User.prototype.getUserProfile = function(username, displayUserData, userFail){
   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    displayUserData(response.name, response.location, response.email);
-    console.log(response);
+    displayUserData(response.name, response.location, response.email, response.html_url);
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    userFail(error.responseJSON.message);
   });
 };
 
-User.prototype.getRepos = function(username, displayRepos){
+User.prototype.getRepos = function(username, displayRepos, reposFail){
   $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
-
     for(i=0; i<response.length; i++) {
-      displayRepos(response[i].url, response[i].name, response[i].description);
+      console.log(response);
+      displayRepos(response[i].name, response[i].language, moment(response[i].created_at).format('ll'), response[i].description, response[i].url);
     }
-
     console.log(response);
   }).fail(function(error){
-    console.log(error.responseJSON.message);
+    reposFail(error.responseJSON.message);
   });
 };
 
