5a2a973cdec8d9d999444a1834e54a93dd63ab8e
Sara Jay <jensese@MacBook-Pro-2.local>
2016-10-14 13:10:14 -0700

    Add conditional to api request that allows a user without an api key for github to make a request to show github information.

--- js/user.js
+++ js/user.js
@@ -3,24 +3,44 @@ var apiKey = require('./../.env').apiKey;
 User = function(){
 };
 
-User.prototype.getUserProfile = function(username, displayUserData, userFail){
-  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-    displayUserData(response.name, response.location, response.email, response.html_url);
-  }).fail(function(error){
-    userFail(error.responseJSON.message);
-  });
+User.prototype.getUserProfile = function(username, displayUserData, userFail) {
+  console.log(apiKey);
+  if (apiKey === undefined) {
+    $.get('https://api.github.com/users/' + username).then(function(response){
+      displayUserData(response.name, response.location, response.email, response.html_url);
+    }).fail(function(error){
+      userFail(error.responseJSON.message);
+    });
+  } else {
+    $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+      displayUserData(response.name, response.location, response.email, response.html_url);
+    }).fail(function(error){
+      userFail(error.responseJSON.message);
+    });
+  }
 };
 
-User.prototype.getRepos = function(username, displayRepos, reposFail){
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
-    for(i=0; i<response.length; i++) {
-      console.log(response);
-      displayRepos(response[i].name, response[i].language, moment(response[i].created_at).format('ll'), response[i].description, response[i].url);
-    }
-    console.log(response);
-  }).fail(function(error){
-    reposFail(error.responseJSON.message);
-  });
+
+User.prototype.getRepos = function(username, displayRepos, reposFail) {
+  if (apiKey === undefined) {
+    $.get('https://api.github.com/users/' + username + '/repos').then(function(response){
+      for(i=0; i<response.length; i++) {
+        console.log(response);
+        displayRepos(response[i].name, response[i].language, moment(response[i].created_at).format('ll'), response[i].description, response[i].url);
+      }
+    }).fail(function(error){
+      reposFail(error.responseJSON.message);
+    });
+  } else {
+    $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+      for(i=0; i<response.length; i++) {
+        console.log(response);
+        displayRepos(response[i].name, response[i].language, moment(response[i].created_at).format('ll'), response[i].description, response[i].url);
+      }
+    }).fail(function(error){
+      reposFail(error.responseJSON.message);
+    });
+  }
 };
 
 exports.userModule = User;
